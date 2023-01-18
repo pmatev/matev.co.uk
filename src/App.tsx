7ed678a0101394ad0a563382cf6adff4c53c9db6
Peter Matev <peter@wayve.ai>
2023-01-18 21:13:45 +0000

    re-do create-react-app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="mesh mesh01">
+      <span> Mesh Gradient 01</span>
+    </div>
+  );
+}
+
+export default App;
