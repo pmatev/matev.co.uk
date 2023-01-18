04fd21907a2b00a0fb3fa36b3aeaf349611d17b7
Peter Matev <peter@wayve.ai>
2023-01-18 21:55:55 +0000

    add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pic from './profile.jpg'
 
-function App() {
+import { generateJSXMeshGradient } from "meshgrad";
+
+
+export function App() {
   return (
-    <div className="mesh mesh01">
-      <span> Mesh Gradient 01</span>
+  <div>
+    <div style={generateJSXMeshGradient(7, '#000')} className="background" />
+    <div className="background-shade" />
+    <div>
+      <h1>Peter Matev</h1>
+      <img className="avatar" src={pic} />
+      <p>Engineering Leader @ Wayve, Senior Engineer @ Ex-Deliveroo, Ex-Founder, Ex Film Maker</p>
+      <p>Contact me: <code><a href="mailto:peter@matev.co.uk">peter@matev.co.uk</a></code></p>
+
+      <div style={{padding: '1rem'}}></div>
+      <p>I like to make things. You can find some of them here.</p>
     </div>
-  );
+  </div>);
 }
 
 export default App;
